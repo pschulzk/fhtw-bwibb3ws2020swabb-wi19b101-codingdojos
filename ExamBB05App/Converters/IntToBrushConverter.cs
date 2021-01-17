7ac7ad273e58b0d892e8428fc9e1b6e5a125af97
Philip Schulz-klingauf <psk@schulz-klingauf.de>
2021-01-17 15:16:07 +0100

    added IntToBrushConverter

---
+++ ExamBB05App/Converters/IntToBrushConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ExamBB05App.Converters
+{
+    class IntToBrushConverter : IValueConverter
+    {
+        readonly Brush yellow = new SolidColorBrush(Colors.Yellow);
+        readonly Brush green = new SolidColorBrush(Colors.Green);
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((int) value > 1)
+            {
+                return green;
+            }
+            else {
+                return yellow;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
